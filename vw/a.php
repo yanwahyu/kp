9330d6f035c4f8f1c2d72f07b3d90268cc1760bc
yanwahyu <wayantkd@gmail.com@users.noreply.github.com>
2020-05-08 13:21:00 +0800

    Article view

---
+++ vw/a.php
@@ -0,0 +1,21 @@
+<ul class="breadcrumbs"><li><a href="https://kowpi.com">Home</a></li><li><a href="https://kowpi.com/category/<?=$d['as']['cats']?>"><?=$d['as']['catNama']?></a></li></ul>
+<article class="ac">
+<h1 class="t"><?=$d['as']['nama']?></h1>
+<div class="i">
+<span>&#9998;</span> <?=ucwords(str_replace('-',' ',$d['as']['auth']))?>, <?= date('d M Y', strtotime($d['as']['date']));?>
+</div>
+<?=$d['as']['cont']?>
+</article>
+<div class="wt"><h3 class="wtt">BACA JUGA</h3></div>
+<div class="w">
+<div class="wp">
+<h2 class="wpt"><a href="https://demo.mekshq.com/typology/?p=101">Something that I’ve never told anyone before</a></h2>         
+<div class="wpi">March 24, 2020</div>
+<div class="wpl">w</div>
+</div>
+<div class="wp">
+<h2 class="wpt"><a href="https://demo.mekshq.com/typology/?p=101">Something that I’ve never told anyone before</a></h2>         
+<div class="wpi">March 24, 2020</div>
+<div class="wpl">w</div>
+</div>
+</div>
