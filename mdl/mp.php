ba4882d2b60560f3fc541270426dba7a3014a8fd
yanwahyu <wayantkd@gmail.com@users.noreply.github.com>
2020-05-08 14:28:13 +0800

    clean and make short variable

--- mdl/mp.php
+++ mdl/mp.php
@@ -1,44 +1,34 @@
-<?php 
-// class post
-
-class mp {private $db;private $publish = 'stat LIKE 1';public function __construct(){$this->db=new DB;}
-
-public function as($slug){ //article_single
-   $this->db->query('SELECT p.nama,p.des,p.auth,p.date,p.cont,p.cats,p.slug FROM'.XP.'WHERE slug=:slug AND '.$this->publish);
-   $this->db->bind('slug',$slug);
-   return $this->db->ef();
+<?php class mp {private$d;private$publish=' stat LIKE 1 ';function __construct(){$this->d=new DB;}
+function as($s){ //article_single
+   $this->d->query('SELECT p.nama,p.des,p.auth,p.date,p.cont,p.cats,p.slug FROM'.XP.'WHERE slug=:s AND'.$this->publish);
+   $this->d->b('s',$s);
+   return $this->d->ef();
 }
-
-public function ac(){ // article count
-   $this->db->query('SELECT count(nama) FROM'.XP.'WHERE '.$this->publish);
-   return $this->db->efc();
+function ac(){ // article count
+   $this->d->query('SELECT count(nama) FROM'.XP.'WHERE'.$this->publish);
+   return $this->d->efc();
 }
-
-public function aa($os){ // article all
-   
-   $this->db->query('SELECT * FROM'.XP.'WHERE '.$this->publish.' ORDER BY date DESC LIMIT '.PL.' OFFSET '.$os);
-   return $this->db->efa();
+function aa($o){ // article all
+   $this->d->query('SELECT * FROM'.XP.'WHERE'.$this->publish.'ORDER BY date DESC LIMIT '.PL.' OFFSET '.$o);
+   return $this->d->efa();
 }
-
-public function aabc($name,$offset){ // article all by cats
-   $this->db->query('SELECT p.nama,p.des,p.auth,p.date,p.cont,p.cats,p.slug from'.XP.'p left join'.XC.'c on c.slug = p.cats WHERE c.slug = :name and p.'.$this->publish.' ORDER BY date DESC LIMIT '.PL.' OFFSET '.$offset);
-   $this->db->bind('name',$name);
-   return $this->db->efa();
+function aabc($s,$o){ // article all by cats
+   $this->d->query('SELECT p.nama,p.des,p.auth,p.date,p.cont,p.cats,p.slug from'.XP.'p left join'.XC.'c on c.slug = p.cats WHERE c.slug = :s and p.'.$this->publish.'ORDER BY date DESC LIMIT '.PL.' OFFSET '.$o);
+   $this->d->b('s',$s);
+   return $this->d->efa();
 }
-public function acbc($name){ // article count by cats
-   $this->db->query('SELECT count(p.nama) from'.XP.'p left join'.XC.'c on c.slug = p.cats WHERE c.slug = :name and p.'.$this->publish);
-   $this->db->bind('name',$name);
-   return $this->db->efc();
+function acbc($n){ // article count by cats
+   $this->d->query('SELECT count(p.id) from'.XP.'p left join'.XC.'c on c.slug = p.cats WHERE c.slug = :n and p.'.$this->publish);
+   $this->d->b('n',$n);
+   return $this->d->efc();
 }
-
-public function ap(){ // article popular
-   $this->db->query('select id,name,slug,cont from'.XP.'WHERE FIND_IN_SET(`id`, (select CONCAT(",", popular, ",") from'.XS.'where id = 1))');
-   return $this->db->efa();
-   }
-
-public function asm(){ // article sitemap
-   $this->db->query('SELECT p.nama,p.date,p.slug FROM'.XP.'WHERE '.$this->publish.' ORDER BY date DESC LIMIT 20');
-   return $this->db->efa();
+function ap(){ // article popular
+   $this->d->query('select id,name,slug,cont from'.XP.'WHERE FIND_IN_SET(`id`, (select CONCAT(",", popular, ",") from'.XS.'where id = 1))');
+   return $this->d->efa();
+}
+function asm(){ // article sitemap
+   $this->d->query('SELECT p.nama,p.date,p.slug FROM'.XP.'WHERE'.$this->publish.'ORDER BY date DESC LIMIT 20');
+   return $this->d->efa();
 }
 
 }
