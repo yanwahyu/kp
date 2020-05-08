751584fb68acd372203995126b26c18e135783a3
yanwahyu <wayantkd@gmail.com@users.noreply.github.com>
2020-05-08 13:24:30 +0800

    Pagination view

---
+++ vw/p.php
@@ -0,0 +1,6 @@
+<?php if($d['tp']!=1){
+echo'<div class="p atc">';echo$d['pg']!=1?'<a href="/'.$d['l'].'" class="pg">Halaman Awal</a>':'<span class="pg">Halaman Awal</span>';$t=2;$s=($d['pg']>$t)?$d['pg']-$t:2;$s=(2>$d['tp']-$s)?$d['tp']-2:$s;$s=($s==1)?2:$s;$e=($d['pg']<($d['tp']-$t))?$d['pg']+$t:$d['tp'];$e=($d['pg']<3)?5:$e;$e=($d['tp']<2*2)?$d['tp']:$e;$s=($d['tp']<2*2)?2:$s;
+for($i=$s;$i<=$e;$i++){
+echo $d['pg']==$i?'<span class="pg on">'.$i.'</span>':'<a href="/'.$d['l'].$d['pl'].$i.'" class="pg">'.$i.'</a>';echo $d['pg']!=$d['tp']&&$d['tp']!=0&&$d['tp']>5?'<a href="/'.$d['l'].$d['tp'].'':'" class="pg">Halaman Terakhir</a>';
+}
+echo '</div>';}?>
